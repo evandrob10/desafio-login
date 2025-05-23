6bbcf653c4a231ee0682ff228568424af56c2269
Evandro Barros <evandrobarrosb98@gmail.com>
2025-05-23 15:55:20 -0300

    update url itens static

--- javascript/script.js
+++ javascript/script.js
@@ -4,10 +4,10 @@ function darkButton() {
     const linkStyleElement = document.querySelector('head > link');
     if (linkStyleElement.getAttribute('href').includes('styleLight')) {
         buttonDark.setAttribute('fill','#FFF')
-        return linkStyleElement.setAttribute('href', '/css/styleDark.css');
+        return linkStyleElement.setAttribute('href', './css/styleDark.css');
     }
     buttonDark.setAttribute('fill','#000');
-    return linkStyleElement.setAttribute('href', '/css/styleLight.css');
+    return linkStyleElement.setAttribute('href', './css/styleLight.css');
 }
 
 buttonDark.addEventListener('click', (event) => {
