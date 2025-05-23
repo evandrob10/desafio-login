82d4ea2ee832edeac0bce95602778ea11c903d50
Evandro Barros <evandrobarrosb98@gmail.com>
2025-05-23 14:54:42 -0300

    criado a estrutura basica sem responsivo

---
+++ javascript/script.js
@@ -0,0 +1,18 @@
+const buttonDark = document.querySelector('#buttonDark');
+
+function darkButton() {
+    const linkStyleElement = document.querySelector('head > link');
+    if (linkStyleElement.getAttribute('href').includes('styleLight')) {
+        buttonDark.setAttribute('fill','#FFF')
+        return linkStyleElement.setAttribute('href', '/css/styleDark.css');
+    }
+    buttonDark.setAttribute('fill','#000');
+    return linkStyleElement.setAttribute('href', '/css/styleLight.css');
+}
+
+buttonDark.addEventListener('click', (event) => {
+    event.preventDefault();
+    darkButton();
+})
+
+
